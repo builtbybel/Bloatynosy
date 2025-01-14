e68fcd900a95d0cee8a14afe0c28afabbaf57460
Belim <belim@builtbybel.com>
2025-01-14 17:18:50 +0100

    Update AssemblyInfo.cs

--- src/Bloatynosy/Properties/AssemblyInfo.cs
+++ src/Bloatynosy/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Buildnummer
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.10.500")]
-[assembly: AssemblyFileVersion("1.0.10.500")]
+[assembly: AssemblyVersion("1.0.20.522")]
+[assembly: AssemblyFileVersion("1.0.20.522")]
